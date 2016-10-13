94990db55e04d7ef45fef1040e7aa110acc5bc43
ngoraymond <ngoraymond7@gmail.com>
2016-10-13 10:03:42 -0700

    yes

--- Chick.java
+++ Chick.java
@@ -1,4 +1,25 @@
-class Chick //implements Animal 
+class Chick implements Animal 
 {     
      //your code here
+	 private String myType;     
+     private String mySound;  
+     public Chick(String type, String sound)     {         
+         myType = type;         
+         mySound = sound;     
+     }     
+     public Chick()     {         
+         myType = "unknown";         
+         mySound = "unknown";     
+     }  
+     public Chick(String type, String sound, String sound2)  {   
+         myType = type;
+         if(Math.random()<0.5)  {     
+        	 mySound = sound;
+         }else{
+		  	 mySound=sound2;
+         }
+
+     }    
+     public String getSound(){return mySound;}     
+     public String getType(){return myType;} 
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -2,5 +2,9 @@
 void setup()
 {
 	//your variable declarations here
+    //Cow c = new Cow("Cow", "moo");   
+    Farm c = new Farm();
+    c.animalSounds();
+
 }
 
